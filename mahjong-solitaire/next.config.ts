015c522a4365c503a73087d627b320973ee511bf
damir9955 <79534425+damir9955@users.noreply.github.com>
2026-09-07 16:00:44 +0500

    Delete mahjong-solitaire directory

--- mahjong-solitaire/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Строгий режим выключен: DOM-анимации полёта плиток в лоток
-  // рассчитаны на однократное срабатывание эффектов.
-  reactStrictMode: false,
-};
-
-export default nextConfig;
